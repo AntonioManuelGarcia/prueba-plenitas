22b6259e7690e5982c8fba7f64400aa4b36b7e7d
AntonioManuelGarcia <wiladrow@gmail.com>
2025-05-16 12:00:48 +0200

    Add: admin fields

--- proyecto/core/admin.py
+++ proyecto/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- proyecto/dispositivos/admin.py
+++ proyecto/dispositivos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Dispositivo
 
-# Register your models here.
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'ip', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('name', 'ip', 'user__email')
+    raw_id_fields = ('user',)
+    list_editable = ('is_active',)
+    date_hierarchy = 'created_at'
+
+admin.site.register(Dispositivo, DispositivoAdmin)
